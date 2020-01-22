03cd1ae3ba900a99b7949d512f8c5258f6bdb8b5
pokefanleoretrogames <pokefanleoretrogames@users.github.com>
2020-01-22 07:10:49 -0600

    mas

--- js/scripts.js
+++ js/scripts.js
@@ -72,5 +72,43 @@ window.fbAsyncInit = function() {
   };
 
 
+$(document).ready(function(){
+    $(".ir-arriba").click(function(){
+        $("body, html").animate({
+            scrollTop: "0px"
+        }, 500);
+    });
+    
+    $(window).scroll(function(){
+        if( $(this).scrollTop() > 0){
+                $(".ir-arriba").slideDown(300);
+           } else {
+                $(".ir-arriba").slideUp(300);
+           }
+    });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
