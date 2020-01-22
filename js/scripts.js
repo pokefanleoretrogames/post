60f10ae01d7ede91344cb66a0bb98a3369b0c8e7
pokefanleoretrogames <pokefanleoretrogames@users.github.com>
2020-01-21 19:50:40 -0600

    Inicio

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main() {
+    $(".menu-bar").click(function () {
+        if (contador == 1) {
+            $("nav").animate({
+                left: '0'
+            });
+            contador = 0;
+        } else {
+            contador = 1;
+            $("nav").animate({
+                left: '-100%'
+            });
+        }
+    });
+}
+
+// MOSTRAMOS Y OCULTAMOS SUBMENUS
+
+$(".sub-menu").click(function () {
+    $(this).children(".menu-hijo").slideToggle();
+});
+
+$(".menu-hijo").click(function (p) {
+    p.stopPropagation();
+})
+
+
+// SCRIPTS DE SLIDE
+
+var swiper = new Swiper('.swiper-container', {
+    grabCursor: true,
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    freeMode: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+// script de comentarios
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId            : 'your-app-id',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v5.0'
+    });
+  };
+
+
+
+
